d50604d5b1cf170041982751e62fdbfe860351b6
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-16 20:17:56 +0100

    Created a function called get_args() that handles with the make command and returns it. Alson added if __name__ == "__main__

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import argparse
 from datetime import date
-# creating the main parser
-master_parser = argparse.ArgumentParser()
 
-# creating subparsers
-subparsers = master_parser.add_subparsers(
-    dest="command", required=True, help="Create help")  # add helpful help
 
-# create the make_task subcommand
-make_task_parser = subparsers.add_parser(
-    "make", help="Makes a task with provided information")
-make_task_parser.add_argument(
-    "-n", "--name", required=True, help="Name of the task")
-make_task_parser.add_argument("-p", "--priority", required=True, type=int, choices=[
-                              1, 2, 3], help="priority of the task [1,2,3], 1 is low priority, 3 is high")
-make_task_parser.add_argument(
-    "-d", "--deadline", required=False, default=None, help='Deadline: time until due in - days')
+def get_args():
+    # creating the main parser
+    master_parser = argparse.ArgumentParser()
 
-args = master_parser.parse_args()
+    # creating subparsers
+    subparsers = master_parser.add_subparsers(
+        dest="command", required=True, help="Create help")  # add helpful help
 
-if args.command == "make":
-    print(
-        f"trying to create task named {args.name} with priority {args.priority} and deadline {args.deadline} days")
+    # create the make_task subcommand
+    make_task_parser = subparsers.add_parser(
+        "make", help="Makes a task with provided information")
+    make_task_parser.add_argument(
+        "-n", "--name", required=True, help="Name of the task")
+    make_task_parser.add_argument("-p", "--priority", required=True, type=int, choices=[
+        1, 2, 3], help="priority of the task [1,2,3], 1 is low priority, 3 is high")
+    make_task_parser.add_argument(
+        "-d", "--deadline", required=False, default=None, help='Deadline: time until due in - days')
+
+    return master_parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.command == "make":
+        print(
+            f"trying to create task named {args.name} with priority {args.priority} and a deadline of {args.deadline} days")

c380583972506482827efb8599deb5323f7f357e
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-16 20:39:30 +0100

    Added arg handling for finish task functionality.

--- main.py
+++ main.py
@@ -14,11 +14,17 @@ def get_args():
     make_task_parser = subparsers.add_parser(
         "make", help="Makes a task with provided information")
     make_task_parser.add_argument(
-        "-n", "--name", required=True, help="Name of the task")
+        "-n", "--name", required=True, help="Name of the task to be created")
     make_task_parser.add_argument("-p", "--priority", required=True, type=int, choices=[
         1, 2, 3], help="priority of the task [1,2,3], 1 is low priority, 3 is high")
     make_task_parser.add_argument(
         "-d", "--deadline", required=False, default=None, help='Deadline: time until due in - days')
+
+    # create the finish_task subcommand
+    finish_task_parser = subparsers.add_parser(
+        "finish", help="Finish a task of the provided name")
+    finish_task_parser.add_argument(
+        "-n", "--name", required=True, help="Name of the task to be finished")
 
     return master_parser.parse_args()
 
@@ -28,3 +34,6 @@ if __name__ == "__main__":
     if args.command == "make":
         print(
             f"trying to create task named {args.name} with priority {args.priority} and a deadline of {args.deadline} days")
+    elif args.command == "finish":
+        print(
+            f"trying to finish task named {args.name}")
